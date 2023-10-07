dc5041f516b88177b9c1720aa9d23e80ef301cbb
ttokaev <toktim1709@bk.ru>
2023-10-07 10:57:44 +0300

    fix mistake

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -12,9 +12,13 @@ public class Main {
         service.createUsersTable();
 
         service.saveUser("Name1", "LastName1", (byte) 20);
+        System.out.println("User с именем Ц Name1 добавлен в базу данных )");
         service.saveUser("Name2", "LastName2", (byte) 25);
+        System.out.println("User с именем Ц Name2 добавлен в базу данных )");
         service.saveUser("Name3", "LastName3", (byte) 31);
+        System.out.println("User с именем Ц Name3 добавлен в базу данных )");
         service.saveUser("Name4", "LastName4", (byte) 38);
+        System.out.println("User с именем Ц Name4 добавлен в базу данных )");
 
         service.removeUserById(1);
         System.out.println(service.getAllUsers());
